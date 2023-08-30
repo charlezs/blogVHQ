b9a67f002499f506e7a8cc9854535803c2244c55
Charles-Nyabeze <charlesnnyabeze@gmail.com>
2023-08-30 16:47:19 -0400

    testing

---
+++ src/app/data.tsx
@@ -0,0 +1,10 @@
+// src/app/blogData.ts
+export const blogs = [
+  { id: "1", title: "First Blog", content: "Content of the first blog post." },
+  {
+    id: "2",
+    title: "Second Blog",
+    content: "Content of the second blog post.",
+  },
+  { id: "3", title: "Third Blog", content: "Content of the third blog post." },
+];
