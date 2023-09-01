e68c0b6a387a56319b46bc84d07584a90b1814fb
Charles-Nyabeze <charlesnnyabeze@gmail.com>
2023-09-01 03:39:06 -0400

    heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
+import Footer from "./components/Footer";
 import Main from "./components/Main";
 import Navbar from "./components/Navbar";
-import { blogs } from "./data";
-import Link from "next/link";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Main />
+      <Footer />
     </div>
   );
 };
